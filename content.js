94278a701241d7f75e8dd7222bebb5a7eb3f08c0
atlas <atlas@47ee2d63-ef80-8740-acc3-37f991f2bb75>
2011-11-22 12:59:43 +0000

    基本可用版
    git-svn-id: svn://localhost/trunk/cssviewer@141 47ee2d63-ef80-8740-acc3-37f991f2bb75

--- content.js
+++ content.js
@@ -1,99 +1,194 @@
+;(function(window, document){
 
 function CssViewer(){
-    this._id = +new Date;
     this._isStart = false;
     
-    this.onDocumentMouseOver = function(e){
+	var WRAPER_ID = '__css_viewer_wraper__';
+	var WRAPER_STYLE_TEXT = 'pointer-events: none; position: absolute; display: none; border: red dotted 2px; color: red; z-index: 2147483647; ';
+	
+	var POPUPBOX_ID = '__css_viewer_popupbox__';
+	var POPUPBOX_CSS_STYLE = '';
+	var POPUPBOX_HTML_TEMPLATE = '\
+<% for(var i in rules){ %>\
+	<div class="__css_viewer_header"><%=i %></div>\
+	<div class="__css_viewer_content">\
+		<% \
+			var styles = rules[i].split(";");\
+			var param, index;\
+			for(var s in styles){ \
+				param = styles[s];\
+				index = param.indexOf(":");\
+		%>\
+		<div class="__css_viewer_item">\
+			<div class="__css_viewer_key"><%=param.substring(0, index) %></div>\
+			<div class="__css_viewer_value"><%=param.substring(index) %></div>\
+		</div>\
+		<% } %>\
+	</div>\
+<% } %>';
+	
+	HTMLElement.prototype.css = function(style){
+		var el = this;
+		for(var i in style){
+			el.style[i] = style[i];
+		}
+	}
+	
+	var templateCache = {};
+      
+    var template = function(str, data){
+        var fn = !/\W/.test(str) ?
+          templateCache[str] = templateCache[str] ||
+            template(document.getElementById(str).innerHTML) :
+          new Function("obj",
+            "var p=[],print=function(){p.push.apply(p,arguments);};" +
+            "with(obj){p.push('" +
+            str
+              .replace(/[\r\t\n]/g, " ")
+              .split("<%").join("\t")
+              .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+              .replace(/\t=(.*?)%>/g, "',$1,'")
+              .split("\t").join("');")
+              .split("%>").join("p.push('")
+              .split("\r").join("\\'")
+          + "');}return p.join('');");
+        return data ? fn( data ) : fn;
+    };
+	
+	var getElementDesName = function(el){
+		if(el.id){
+			return '#' + el.id;
+		}else if(el.className){
+			return el.className.replace(/([\w\-_]+)/g, '.$1');
+		}else{
+			return el.tagName;
+		}
+	}
+	
+	var getWraper = function(createFlag){
+		var wraper = document.getElementById(WRAPER_ID);
+		if(!wraper && createFlag){
+			wraper = document.createElement('div');
+			wraper.id = WRAPER_ID;
+			wraper.style.cssText = WRAPER_STYLE_TEXT;
+			document.body.appendChild(wraper);
+		}
+		return wraper;
+	}
+	
+	var removeWraper = function(){
+		var wraper = getWraper();
+		if(wraper){
+			document.body.removeChild(wraper);
+		}
+	}
+	
+	var getScrollTop = function(){
+		return Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+	}
+	
+	var getScrollLeft = function(){
+		return Math.max(document.documentElement.scrollLeft, document.body.scrollLeft);
+	}
+	
+	var getComputedStyle = function(el){
+		var styleSheets = document.styleSheets,
+			styleList = {}, 
+			sheet, rules, rule, selector, style;
+		for(var i = 0, slen = styleSheets.length; i < slen; i++){
+			sheet = styleSheets[i];
+			if(!sheet.rules){
+				continue;
+			}
+			rules = sheet.rules;
+			for(var j = 0, rlen = rules.length; j < rlen; j++){
+				rule = rules[j];
+				selector = rule.selectorText;
+				if(el.webkitMatchesSelector(selector)){
+					style = rule.style.cssText;
+					styleList[selector] = style;
+				}
+			}
+		}
+		return styleList;
+	}
+	
+	var getPopupBox = function(){
+		var popup = document.getElementById(POPUPBOX_ID);
+		if(!popup){
+			popup = document.createElement('div');
+			popup.id = POPUPBOX_ID;
+			document.body.appendChild(popup);
+		}
+		return popup;
+	}
+	
+	var showPopupBox = function(x, y){
+		var popup = getPopupBox();
+		popup.css({
+			top: x + 'px',
+			left: y + 'px',
+			display: 'block'
+		});
+	}
+	
+    var onDocumentMouseOver = function(e){
         var target = e.target;
         var rect = target.getBoundingClientRect();
-        var wraper = CssViewer.getWraper(true);
-        wraper.innerHTML = CssViewer.getElementDesName(target);
-        CssViewer.css(wraper, {
+        var wraper = getWraper(true);
+		var scrollTop = getScrollTop();
+		var scrollLeft = getScrollLeft();
+        wraper.innerHTML = getElementDesName(target);
+        wraper.css({
             'width': rect.width + 'px',
             'height': rect.height + 'px',
-            'top': rect.top - 2 + 'px',
-            'left': rect.left - 2 + 'px',
+            'top': rect.top + scrollTop - 2 + 'px',
+            'left': rect.left + scrollLeft - 2 + 'px',
             'display': 'block'
         });
+		var styleList = getComputedStyle(target);
+		var popup = getPopupBox();
+		var html = template(POPUPBOX_HTML_TEMPLATE, {rules: styleList});
+		popup.innerHTML = html;
+		showPopupBox(e.pageX, e.pageY);
     }
-}
-
-CssViewer.prototype = {
-    start: function(){
+	
+	this.start = function(){
         if(!this._isStart){
             this._isStart = true;
-            CssViewer.getDoc().addEventListener('mouseover', this.onDocumentMouseOver, false);
+            document.addEventListener('mouseover', onDocumentMouseOver, false);
         }
-    },
-    stop: function(){
+    }
+	
+	this.stop = function(){
         if(this._isStart){
             this._isStart = false;
-            CssViewer.removeWraper();
-            CssViewer.getDoc().removeEventListener('mouseover', this.onDocumentMouseOver, false);
+            removeWraper();
+            document.removeEventListener('mouseover', onDocumentMouseOver, false);
         }
-    },
-    isStart: function(){
+    }
+    this.isStart = function(){
         return this._isStart;
     }
 }
 
-CssViewer.css = function(el, style){
-    for(var i in style){
-        el.style[i] = style[i];
-    }
-}
+(function(){
+	if(!window.getCssViewer){
+		window.getCssViewer = function(){
+			if(!window.__css_viewer){
+				window.__css_viewer = new CssViewer();
+			}
+			return window.__css_viewer;
+		}
+	}
+	var viewer = window.getCssViewer();
+	if(viewer.isStart()){
+		viewer.stop();
+	}else{
+		viewer.start();
+	}
+})();
 
-CssViewer.getWraper = function(createFlag){
-    var doc = CssViewer.getDoc();
-    var wraper = doc.getElementById('__css_viewer_wraper__');
-    if(!wraper && createFlag){
-        wraper = doc.createElement('div');
-        wraper.id = '__css_viewer_wraper__';
-        wraper.style.cssText = 'pointer-events: none; position: absolute; display: none; border: red dotted 2px; color: red;';
-        doc.body.appendChild(wraper);
-    }
-    return wraper;
-}
-
-CssViewer.removeWraper = function(){
-    var wraper = CssViewer.getWraper();
-    if(wraper){
-        CssViewer.getDoc().body.removeChild(wraper);
-    }
-}
-CssViewer.getElementDesName = function(el){
-    if(el.id){
-        return '#' + el.id;
-    }else if(el.className){
-        return el.className.replace(/([\w\-_]+)/g, '.$1');
-    }else{
-        return el.tagName;
-    }
-}
-
-CssViewer.setWin = function(win){
-    CssViewer.__win = win;
-    CssViewer.__doc = win.document;
-}
-
-CssViewer.getWin = function(){
-    return CssViewer.__win;
-}
-
-CssViewer.getDoc = function(){
-    return CssViewer.__doc;
-}
-
-CssViewer.getViewer = function(){
-    var win = CssViewer.getWin();
-    var viewer = win.__css_viewer || (win.__css_viewer = new CssViewer());
-    return viewer;
-}
-
-CssViewer.setWin(window);
-var viewer = CssViewer.getViewer();
-if(viewer.isStart()){
-    viewer.stop();
-}else{
-    viewer.start();
-}
+//end coding
+})(window, document);
+//////////////////////////////////
